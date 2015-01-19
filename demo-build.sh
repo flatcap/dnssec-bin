4bf7aa4a7bf9b07738d05964221c0bea3886b3e9
Richard Russon (flatcap) <richard.russon@gmail.com>
2015-01-19 12:40:12 +0000

    working date demo

--- demo-build.sh
+++ demo-build.sh
@@ -6,27 +6,27 @@ ZONE=russon.org
 rm -fr $KEYDIR
 mkdir -p $KEYDIR
 
+P=$(date -d "Jul 14 2014" "+%Y%m%d%H%M%S")
+A=$(date -d "Jul 16 2014" "+%Y%m%d%H%M%S")
+I=$(date -d "Dec 16 2014" "+%Y%m%d%H%M%S")
+D=$(date -d "Dec 18 2014" "+%Y%m%d%H%M%S")
+
+# Create a Key Signing Key (KSK)
+/usr/sbin/dnssec-keygen -P $P -A $A -I $I -D $D -a NSEC3RSASHA1 -b 4096 -n ZONE -K $KEYDIR -f KSK $ZONE
+
+P=$(date -d "Dec 14 2014" "+%Y%m%d%H%M%S")
+A=$(date -d "Dec 16 2014" "+%Y%m%d%H%M%S")
+I=$(date -d "Jun 16 2015" "+%Y%m%d%H%M%S")
+D=$(date -d "Jun 18 2015" "+%Y%m%d%H%M%S")
+
+# Create a Key Signing Key (KSK)
+/usr/sbin/dnssec-keygen -P $P -A $A -I $I -D $D -a NSEC3RSASHA1 -b 4096 -n ZONE -K $KEYDIR -f KSK $ZONE
+
 bin/dnssec-keygen russon.org 2014 09
 bin/dnssec-keygen russon.org 2014 10
 bin/dnssec-keygen russon.org 2014 11
 bin/dnssec-keygen russon.org 2014 12
 bin/dnssec-keygen russon.org 2015 01
-
-P=$(date -d "jul 4 2014" "+%Y%m%d%H%M%S")
-A=$(date -d "jul 6 2014" "+%Y%m%d%H%M%S")
-I=$(date -d "dec 6 2014" "+%Y%m%d%H%M%S")
-D=$(date -d "dec 8 2014" "+%Y%m%d%H%M%S")
-
-# Create a Key Signing Key (KSK)
-/usr/sbin/dnssec-keygen -P $P -A $A -I $I -D $D -a NSEC3RSASHA1 -b 4096 -n ZONE -K $KEYDIR -f KSK $ZONE
-
-P=$(date -d "dec 4 2014" "+%Y%m%d%H%M%S")
-A=$(date -d "dec 6 2014" "+%Y%m%d%H%M%S")
-I=$(date -d "jun 6 2015" "+%Y%m%d%H%M%S")
-D=$(date -d "jun 8 2015" "+%Y%m%d%H%M%S")
-
-# Create a Key Signing Key (KSK)
-/usr/sbin/dnssec-keygen -P $P -A $A -I $I -D $D -a NSEC3RSASHA1 -b 4096 -n ZONE -K $KEYDIR -f KSK $ZONE
 
 ls -l $KEYDIR
 
--- demo-run.sh
+++ demo-run.sh
@@ -6,22 +6,19 @@ ZONE=russon.org
 rm -fr 201[45]*/
 
 START=$(date "+%s" -d "10 Sep 2014 10:00")
-# date -d @$START
-
 END=$(date "+%s" -d "20 Jan 2015")
-# date -d @$END
 
 for ((i = $START; i < $END; i += 86400)); do
 	DIR=$(date -d @$i "+%Y%m%d")
 	DATE=$(date "+%m%d%H%M%Y" -d @$i)
-	date $DATE
-	# echo $DIR $DATE
-	# date -d @$i
 	echo
-	dnssec-signzone -S -o $ZONE -K $KEYDIR ${ZONE}.db
+	date $DATE
+	SALT=$(head -c 1000 /dev/random | sha1sum | cut -b 1-16)
+	dnssec-signzone -3 $SALT -S -o $ZONE -K $KEYDIR ${ZONE}.db
+	dnssec-dsfromkey -K $DIR -f ${ZONE}.db.signed ${ZONE} > dsset-${ZONE}.
 	mkdir $DIR
-	dnssec-dsfromkey -K $DIR -f ${ZONE}.db.signed ${ZONE} >  dsset-${ZONE}.
 	mv ${ZONE}.db.signed dsset-${ZONE}. $DIR
 done
 
+hwclock --hctosys
 
