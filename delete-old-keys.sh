5c1ba8d5378813c385add4d77e4224744671526a
Richard Russon <rich@flatcap.org>
2015-04-22 15:33:55 +0100

    drop junk

--- delete-old-keys.sh
+++ delete-old-keys.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
+PATH="/usr/bin:/usr/sbin"
+
+set -o errexit	# set -e
+set -o nounset	# set -u
+
 shopt -s nullglob
 
-DATE_NOW=$(date +%Y%m%d%H%M%S)
+renice --priority 19 --pid $$ > /dev/null
+ionice --class 3     --pid $$ > /dev/null
 
-for f in "$@"; do
-	FILE_DATE=$(awk '/Delete/{ print $3 ? $3 : $2 }' $f)
-	if [ -z "$f" ]; then
-		echo Ignoring: $f
+cd /var/named
+
+KEYDIR="keys"
+
+[ -d "$KEYDIR" ] || exit 0
+
+FILES="$(find $KEYDIR -type f)"
+
+DATE_NOW=$(date "+%Y%m%d%H%M%S")
+for f in $FILES; do
+	EXPIRY=$(awk '/Delete/{ print $3 ? $3 : $2 }' $f)
+	if [ -z "$EXPIRY" ]; then
+		echo Ignore: $f
 		continue
 	fi
 
-	if [ $DATE_NOW -gt $FILE_DATE ]; then
+	if [ $DATE_NOW -gt $EXPIRY ]; then
 		echo Delete: $f
-		# rm -f "$f"
+		rm -f "$f"
 	else
 		echo Safe: $f
 	fi
