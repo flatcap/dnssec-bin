4e1aa3be893be2d9eb3d2f5cf4325320c98a1248
Richard Russon <rich@flatcap.org>
2015-04-27 15:24:03 +0100

    refactor into functions

--- cron.sh
+++ cron.sh
@@ -22,31 +22,16 @@ cd /var/named
 
 # ----------------------------------------------------------
 
-X=${1:-$(date "+%Y%m%d%H%M%S")}
-
-if [[ ! "$X" =~ ^[0-9]{14}$ ]]; then
-	echo "Invalid argument: $X"
-	exit 1
-fi
-
-YEAR=${X:0:4}
-MONTH=${X:4:2}
-DAY=${X:6:2}
-H=${X:8:2}
-M=${X:10:2}
-S=${X:12:2}
-
-echo "Cron: for $YEAR-$MONTH-$DAY $H:$M:$S"
-
-function matching_ksk
+function matching_ksk()
 {
-	[ $# = 1 ] || return 1
+	[ $# = 2 ] || return 1
 
-	local TIMESTAMP=$1
+	local ZONE=$1
+	local TIMESTAMP=$2
 	local FILE
 	local k
 
-	for k in "$DNSSEC_KEY_DIR"/*.key; do
+	for k in "$DNSSEC_KEY_DIR"/K$ZONE*.key; do
 		FILE=$(cat $k)
 		if [[ ! "$FILE" =~ key-signing.*Activate:\ *([0-9]{14}).*Inactive:\ ([0-9]{14}) ]]; then
 			continue
@@ -64,68 +49,151 @@ function matching_ksk
 	return 1
 }
 
+function matching_zsk()
+{
+	[ $# = 2 ] || return 1
 
-if ! matching_ksk $YEAR$MONTH$DAY$H$M$S; then
-	echo no match
-	echo Need to backdate a KSK
-	echo Now: $YEAR $MONTH
-	if [ $MONTH -lt $KSK_MONTH1 ]; then
-		M2=$KSK_MONTH2
-		Y2=$((YEAR-1))
+	local ZONE=$1
+	local TIMESTAMP=$2
+	local FILE
+	local k
+
+	for k in "$DNSSEC_KEY_DIR"/K$ZONE*.key; do
+		FILE=$(cat $k)
+		if [[ ! "$FILE" =~ zone-signing.*Activate:\ *([0-9]{14}).*Inactive:\ ([0-9]{14}) ]]; then
+			continue
+		fi
+
+		local ACTIVATE=${BASH_REMATCH[1]}
+		local INACTIVE=${BASH_REMATCH[2]}
+		if [ $TIMESTAMP -lt $ACTIVATE -o $TIMESTAMP -gt $INACTIVE ]; then
+			continue
+		fi
+
+		echo Matching ZSK: ${BASH_REMATCH[1]} ${BASH_REMATCH[2]} $k
+		return 0
+	done
+	return 1
+}
+
+
+function current_ksk()
+{
+	[ $# = 1 ] || return 1
+
+	local ZONE=$1
+
+	# KSK - 6 months
+
+	if ! matching_ksk $ZONE $YEAR$MONTH$DAY$H$M$S; then
+		echo Need to backdate a KSK
+		echo Now: $YEAR $MONTH
+		local M2
+		local Y2
+		if [ $MONTH -le $KSK_MONTH1 ]; then
+			M2=$KSK_MONTH2
+			Y2=$((YEAR-1))
+		else
+			M2=$KSK_MONTH1
+			Y2=$YEAR
+		fi
+		echo Gen KSK for: $ZONE $Y2 $M2
+		generate-ksk $ZONE $Y2 $M2
 	else
-		M2=$KSK_MONTH1
-		Y2=$YEAR
+		echo We can use an existing KSK for zone $ZONE
 	fi
-	echo Gen KSK for: $Y2 $M2
-	generate-ksk russon.org $Y2 $M2
-	exit 0
-else
-	echo match
-fi
 
-exit 0
+	if [ $MONTH$DAY = "${KSK_MONTH1}${SWAP_DAY}" -o $MONTH$DAY = "${KSK_MONTH2}${SWAP_DAY}" ]; then
+		echo Time for a new KSK
+		generate-ksk $ZONE $YEAR $((MONTH+1))
+	fi
+}
+
+function current_zsk()
+{
+	[ $# = 1 ] || return 1
+
+	local ZONE=$1
+
+	# ZSK - 1 month
+
+	if ! matching_zsk $YEAR$MONTH$DAY$H$M$S; then
+		echo Need to backdate a ZSK
+		echo Gen ZSK for: $ZONE $YEAR $MONTH
+		generate-zsk $ZONE $YEAR $MONTH
+	else
+		echo We can use an existing ZSK for zone $ZONE
+	fi
+
+	if [ $DAY = "$SWAP_DAY" ]; then
+		echo Time for a new ZSK
+		generate-zsk $ZONE $YEAR $((MONTH+1))
+	fi
+}
+
+
+function daily_prep()
+{
+	generate-dns-glue
+	generate-root-certs
+	generate-ssh-fingerprint
+	generate-tlsa
+
+	update-serials
+}
+
+function daily_signing()
+{
+	[ $# = 1 ] || return 1
+
+	local ZONE=$1
+
+	# SIGNING - daily
+
+	sign-zone $ZONE
+}
+
+function daily_tidy()
+{
+	# ----------------------------------------------------------
+	# TIDY - daily
+
+	delete-old-keys
+	fix-perms
+	set-to-publish-date "$DNSSEC_KEY_DIR"/*
+}
+
 
 # ----------------------------------------------------------
-# KSK - 6 months
 
-if [ $MONTH$DAY = "${KSK_MONTH1}${SWAP_DAY}" -o $MONTH$DAY = "${KSK_MONTH2}${SWAP_DAY}" ]; then
-	echo Time for a new KSK
-	for d in $DNSSEC_DOMAINS; do
-		generate-ksk $d $YEAR $((MONTH+1))
-	done
+X=${1:-$(date "+%Y%m%d%H%M%S")}
+
+if [[ ! "$X" =~ ^[0-9]{14}$ ]]; then
+	echo "Invalid date: $X"
+	exit 1
 fi
 
-# ----------------------------------------------------------
-# ZSK - 1 month
+YEAR=${X:0:4}
+MONTH=${X:4:2}
+DAY=${X:6:2}
+H=${X:8:2}
+M=${X:10:2}
+S=${X:12:2}
 
-if [ $DAY = "$SWAP_DAY" ]; then
-	echo Time for a new ZSK
-	for d in $DNSSEC_DOMAINS; do
-		generate-zsk $d $YEAR $((MONTH+1))
-	done
-fi
+echo "Cron: for $YEAR-$MONTH-$DAY $H:$M:$S"
 
-# ----------------------------------------------------------
-# SIGNING - daily
-
-generate-dns-glue
-generate-root-certs
-generate-ssh-fingerprint
-generate-tlsa
-
-update-serials
+daily_prep
 
 for d in $DNSSEC_DOMAINS; do
-	sign-zone $d
+	current_ksk $d
+	current_zsk $d
+	daily_signing $d
 done
 
-# ----------------------------------------------------------
-# TIDY - daily
-
-delete-old-keys
-fix-perms
-set-to-publish-date "$DNSSEC_KEY_DIR"/*
+daily_tidy
+echo
 show-keys
+show-signed
 
 # ds-sync.pl
 # reload named
